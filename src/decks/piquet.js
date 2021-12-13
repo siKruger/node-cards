8b74af88d89799d1b47dbfd89d900edc4180fc8e
Simon Krüger <krueger.simon2000@gmail.com>
2021-12-13 21:33:49 +0100

    Update piquet.js

--- src/decks/piquet.js
+++ src/decks/piquet.js
@@ -24,9 +24,37 @@ const generateDeck = () => {
 
 	suits.forEach((suit) => {
 		ranks.forEach((rank) => {
-			cards.push(new Card(suit, rank));
+			cards.push(new Card(suit, rank, getAbsVal(rank)));
 		});
 	});
 
 	return cards;
 };
+
+const getAbsVal = (rank) => {
+    switch (rank.longName) {
+        case "Seven":
+            return 7;
+
+        case "Eight":
+            return 8;
+
+        case "Nine":
+            return 9;
+
+        case "Ten":
+            return 10;
+
+        case "Jack":
+            return 11;
+
+        case "Queen":
+            return 12;
+
+        case "King":
+            return 13;
+
+        case "Ace":
+            return 14;
+    }
+}
