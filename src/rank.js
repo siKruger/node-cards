6f25a4e1426169eb411703ef5eff02aadeb28c59
Simon Krüger <sim.krueger@ostfalia.de>
2022-02-25 12:33:50 +0100

    new property on rank for better handling

--- src/rank.js
+++ src/rank.js
@@ -8,18 +8,22 @@
  * @param longName {string}
  */
 class Rank {
-	constructor(shortName, longName) {
+	constructor(shortName, longName, absoluteVal) {
 		/** @member {string} */
 		this.shortName = shortName;
 
 		/** @member {string} */
 		this.longName = longName;
+
+
+		/** @member {number} deprecated*/
+		this.absoluteVal = absoluteVal;
 	}
 
 	/**
 	 * Returns a human-readable string representation of the rank object
 	 *
-	 * eg. `"<Rank shortName=A longName=Ace>"`
+	 * eg. `"<Rank shortName=A longName=Ace absoluteVal=13>"`
 	 *
 	 * @return {string}
 	 */
